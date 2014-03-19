0cab358a978c2cb544fe5fa004de2b3f0282fc17
sergio1990 <sergeg1990@gmail.com>
2014-03-19 17:10:07 +0200

    -Added competition's classes

---
+++ lib/betfair_api_ng_rails/api/bf/data/competition.rb
@@ -0,0 +1,14 @@
+module BetfairApiNgRails
+  module Api
+    module BF
+      module Data
+        class Competition
+
+          attr_accessor :id,
+                        :name
+
+        end
+      end
+    end
+  end
+end
---
+++ lib/betfair_api_ng_rails/api/bf/data/competition_result.rb
@@ -0,0 +1,15 @@
+module BetfairApiNgRails
+  module Api
+    module BF
+      module Data
+        class CompetitionResult
+
+          attr_accessor :marketCount,
+                        :competitionRegion,
+                        :competition
+
+        end
+      end
+    end
+  end
+end
