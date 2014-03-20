4c25ebd39b4c98de3e63c2ddbe3315a412f492f1
sergio1990 <sergeg1990@gmail.com>
2014-03-20 14:11:51 +0200

    -Added some additional finders to Competition

--- lib/betfair_api_ng_rails/api/bf/data/competition.rb
+++ lib/betfair_api_ng_rails/api/bf/data/competition.rb
@@ -7,8 +7,17 @@ module BetfairApiNgRails
           attr_accessor :id,
                         :name
 
-          def self.fetch(filter: Api::BF::Data::MarketFilter.new, params: {})
+
+          def self.where(filter: Api::BF::Data::MarketFilter.new, params: {})
             BetfairApiNgRails::Api::BF::ORM::Requester.load resource: :competitions, filter: filter, params: params
+          end
+
+          def self.all
+            where
+          end
+
+          def self.find(id)
+            where filter: Api::BF::Data::MarketFilter.new(competition_ids: [id])
           end
 
         end
