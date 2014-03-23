9f61037f98db122f37d91995969be304b1801f44
sergio1990 <sergeg1990@gmail.com>
2014-03-23 07:51:25 +0200

    -Fixed EventTypeResult -Added loading from json time range

--- lib/betfair_api_ng_rails/api/bf/data/time_range.rb
+++ lib/betfair_api_ng_rails/api/bf/data/time_range.rb
@@ -3,9 +3,13 @@ module BetfairApiNgRails
     module BF
       module Data
         class TimeRange < Api::BF::Data::Base
+          include Api::BF::Data::Attributes
 
-          attr_accessor :from,
-                        :to
+          attr_accessor *TIME_RANGE_ATTRS
+
+          def self.from_json(json_row)
+            new TIME_RANGE_ATTRS.inject({}) { |h, a| h[a] = json_row[a.to_s.camelize(:lower)]; h }
+          end
 
         end
       end
--- lib/betfair_api_ng_rails/api/bf/data/time_range_result.rb
+++ lib/betfair_api_ng_rails/api/bf/data/time_range_result.rb
@@ -7,6 +7,13 @@ module BetfairApiNgRails
           attr_accessor :time_range,
                         :market_count
 
+          def self.from_json(json_row)
+            new(
+              time_range: Api::BF::Data::TimeRange.from_json(json_row['timeRange']),
+              market_count: json_row['marketCount'],
+            )
+          end
+
         end
       end
     end
