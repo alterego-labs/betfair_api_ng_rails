3d53444e7e97ae37b48016c227273a97ca88d205
sergio1990 <sergeg1990@gmail.com>
2014-04-16 16:35:57 +0300

    Added SessionManager spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,15 @@
 $TESTING=true
 $:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
 
+require 'rspec/its'
+
 require 'betfair_api_ng_rails'
+
+RSpec.configure do |config|
+  config.mock_with :rspec
+
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+
+  config.order = "random"
+end
