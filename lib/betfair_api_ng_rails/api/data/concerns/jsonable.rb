081c78f0c015393951867a007b3b40fdad3439e6
sergio1990 <sergeg1990@gmail.com>
2014-04-22 13:03:29 +0300

    Completed Jsonable concern
    Ref #18

--- lib/betfair_api_ng_rails/api/data/concerns/jsonable.rb
+++ lib/betfair_api_ng_rails/api/data/concerns/jsonable.rb
@@ -10,7 +10,7 @@ module BetfairApiNgRails
 
           module ClassMethods
             
-            def to_json(data = {})
+            def from_json(data = {})
               return nil unless self.respond_to?(:class_attrs)
               self.new set_attributes(data)
             end
@@ -18,13 +18,22 @@ module BetfairApiNgRails
           private
 
             def set_attributes(data)
-              self.class_attrs.each_with_object({}) do |attrib, h|
-                h[attrib] = data[json_key(attrib)]
+              self.class_attrs.each_with_object({}) do |(name, options), h|
+                h[name] = get_attr_value(data, name, options)
               end
             end
 
             def json_key(attrib)
               attrib.to_s.camelize(:lower)
+            end
+
+            def get_attr_value(data, name, options)
+              j_data = data[json_key(name)]
+              options[:array] ? j_data.map { |r| prepare_value(options[:type], r) } : prepare_value(options[:type], j_data)
+            end
+
+            def prepare_value(type, data)
+              type == String ? data : type.from_json(data)
             end
 
           end
--- spec/betfair_api_ng_rails/api/data/concerns/jsonable_spec.rb
+++ spec/betfair_api_ng_rails/api/data/concerns/jsonable_spec.rb
@@ -2,6 +2,12 @@ require 'spec_helper'
 
 class TestJsonable
   include BetfairApiNgRails::Api::Data::Concerns::Jsonable
+
+  def initialize(args = {})
+    args.each { |attribute, value| self.send("#{attribute}=", value) }
+  end
+
+  attr_accessor :some_attr
 end
 
 describe BetfairApiNgRails::Api::Data::Concerns::Jsonable do
@@ -10,11 +16,11 @@ describe BetfairApiNgRails::Api::Data::Concerns::Jsonable do
 
     subject { TestJsonable }
 
-    describe ".to_json" do
+    describe ".from_json" do
       
       context 'when not defined class attributes' do
         
-        its(:to_json) { is_expected.to eq nil }
+        its(:from_json) { is_expected.to eq nil }
 
       end
 
@@ -27,7 +33,7 @@ describe BetfairApiNgRails::Api::Data::Concerns::Jsonable do
         it "returns new instance with fetched attributes" do
           expect(subject).to receive(:set_attributes).and_return attributes
           expect(subject).to receive(:new).with(attributes)
-          subject.to_json
+          subject.from_json
         end
 
       end
@@ -44,8 +50,109 @@ describe BetfairApiNgRails::Api::Data::Concerns::Jsonable do
 
     describe ".set_attributes" do
       
+      let(:data) { double(:data) }
+
+      before { allow(subject).to receive(:class_attrs).and_return({some_attr: {type: String}}) }
+
+      it "returns hash attributes" do
+        expect(subject).to receive(:get_attr_value).with data, :some_attr, {type: String}
+        subject.send :set_attributes, data
+      end
+
     end
 
+    describe ".get_attr_value" do
+      
+      subject { TestJsonable.send(:get_attr_value, attribs, :some_attr, {type: type, array: is_array}) }
+
+      context 'when fetching data for single valued attr' do
+        
+        let(:attribs)  { {'someAttr' => :value} }
+        let(:is_array) { false }
+
+        context 'and its type is String ' do
+          
+          let(:type) { String }
+
+          it { is_expected.to eq :value }
+
+        end
+
+        context 'and its type is custom' do
+
+          let(:type) { TestJsonable }
+
+          it "calls from_json on proper class" do
+            expect(type).to receive(:from_json).and_return :some_object
+            expect(subject).to eq :some_object
+          end
+          
+        end
+
+      end
+
+      context 'when fetching data for array attr' do
+
+        let(:attribs)  { {'someAttr' => [:value]} }
+        let(:is_array) { false }
+        
+        context 'and its type is custom' do
+          
+          let(:type) { TestJsonable }
+
+          it "calls from_json on proper class" do
+            expect(type).to receive(:from_json).and_return [:some_object]
+            expect(subject).to include(:some_object)
+          end
+
+        end
+
+      end
+
+    end
+
+    describe ".prepare_value" do
+      
+      let(:data) { double(:data) }
+
+      subject { TestJsonable.send(:prepare_value, type, data) }
+
+      context 'when called for String type' do
+        
+        let(:type) { String }
+
+        it { is_expected.to eq data }
+
+      end
+
+      context 'when called for custom type' do
+      
+        let(:type) { TestJsonable }
+
+        before { expect(TestJsonable).to receive(:from_json).and_return(:some_object) }
+
+        it { is_expected.to eq (:some_object) }
+
+      end
+
+    end
+
+  end
+
+  describe "integration tests" do
+
+    before do 
+      allow(TestJsonable).to receive(:class_attrs).and_return({some_attr: {type: String, array: false}})
+    end
+
+    context 'success load object from hashable json' do
+      
+      subject { TestJsonable.from_json({'someAttr' => :value}) }
+
+      its(:some_attr) { is_expected.to eq :value }
+
+    end
+    
   end
 
 end
