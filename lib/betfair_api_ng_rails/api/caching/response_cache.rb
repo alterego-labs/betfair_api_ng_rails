5a54820b8401c98400270d4ab8722011bf6f4281
sergio1990 <sergeg1990@gmail.com>
2014-06-04 16:53:44 +0300

    Completed caching

--- lib/betfair_api_ng_rails/api/caching/response_cache.rb
+++ lib/betfair_api_ng_rails/api/caching/response_cache.rb
@@ -1,9 +1,50 @@
+require 'ostruct'
+require 'json'
+
 module BetfairApiNgRails
   module Api
     module Caching
       class ResponseCache
         include Api::Caching::Helper
+        include Api::Caching::Models
         
+        attr_reader :method, :sig_params
+
+        def initialize(method, params)
+          @method = method
+          @sig_params = createsig(params)
+        end
+
+        def is_cached?
+          !cache_result.nil?
+        end
+
+        def responser
+          Api::Http::Responser.new prepare_response
+        end
+
+        def cache!(response)
+          BetfairCache.create(create_params(response)).expire expire_time
+        end
+
+      private
+
+        def cache_result
+          @_cache_result ||= BetfairCache.where(method: method, params: sig_params).first
+        end
+
+        def prepare_response
+          OpenStruct.new body: cache_result.response, code: '200'
+        end
+
+        def expire_time
+          BetfairApiNgRails.config.cache_expire
+        end
+
+        def create_params(response)
+          { method: method, params: sig_params, response: JSON.dump(response) }
+        end
+
       end
     end
   end
--- lib/betfair_api_ng_rails/api/connection_ext/caching.rb
+++ lib/betfair_api_ng_rails/api/connection_ext/caching.rb
@@ -9,7 +9,11 @@ module BetfairApiNgRails
         def self.included(base)
           base.send :class_eval,  <<-CODE
             def request_with_caching(method, params = {})
-              res = request_without_caching(method, params)
+              if is_caching_on?
+                process_with_caching method, params
+              else
+                return request_without_caching(method, params)
+              end
             end
             alias_method :request_without_caching, :request
             alias_method :request, :request_with_caching
@@ -17,6 +21,29 @@ module BetfairApiNgRails
         end
 
       private
+
+        def is_caching_on?
+          BetfairApiNgRails.config.use_cache
+        end
+
+        def process_with_caching(method, params)
+          cache_service = Api::Caching::ResponseCache.new(method, params)
+          if cache_service.is_cached?
+            return cache_service.responser
+          else
+            return make_real_request(method, params, cache_service)
+          end
+        end
+
+        def make_real_request(method, params, cache_service)
+          res = request_without_caching(method, params)
+          cache_service.cache!(res.result) if need_caching?(res)
+          res
+        end
+
+        def need_caching?(res)
+          !(res.nil? || res.has_error?)
+        end
 
       end
     end
