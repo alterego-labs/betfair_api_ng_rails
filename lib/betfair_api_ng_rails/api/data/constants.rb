a2205c0ead722bbc1b1e4a41e2a0f41299f21ea4
Sergey Gernyak <sergeg1990@gmail.com>
2015-01-20 16:44:55 +0200

    Add Float to attires type list that not serialized

--- lib/betfair_api_ng_rails/api/data/constants.rb
+++ lib/betfair_api_ng_rails/api/data/constants.rb
@@ -2,7 +2,7 @@ module BetfairApiNgRails
   module Api
     module Data
       module Constants
-        ATTRS_AS_IS = [Hash, String, Symbol, Fixnum, NilClass]
+        ATTRS_AS_IS = [Hash, String, Symbol, Fixnum, NilClass, Float]
       end
     end
   end
